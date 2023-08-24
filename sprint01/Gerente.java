08125f1f9993fbb42fd3af83c6914f82e5358616
pedrosch <98292262+pedrobolfe@users.noreply.github.com>
2023-08-24 14:45:23 -0300

    Update Gerente.java

--- sprint01/Gerente.java
+++ sprint01/Gerente.java
@@ -4,18 +4,9 @@ public class Gerente {
 
     //atributos
     private ArrayList<Funcionario> funcionariosCadastrados = new ArrayList<Funcionario>();
-   
-
-
 
     public ArrayList<Funcionario> cadastrarFuncionarios(String nome, String email,String endereco,String telefone,String cargo,double salario){
-
-        
-        
-
         this.funcionariosCadastrados.add(new Funcionario(nome, email, endereco, telefone, cargo, salario));
         return this.funcionariosCadastrados;
-
-    }
-    
+    }  
 }
