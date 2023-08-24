bc070f80a02b06fe285d7f312ee026473ef5f519
pedrosch <98292262+pedrobolfe@users.noreply.github.com>
2023-08-24 14:52:53 -0300

    Update UsaGerente.java

--- sprint01/UsaGerente.java
+++ sprint01/UsaGerente.java
@@ -1,9 +1,10 @@
-public class UsaGerente {
-    public static void main(String[] args) {
+public class UsaGerente { // inicio classe UsaGerente
+    public static void main(String[] args) { // inicio metodo main
+        // criando uma instância da classe Gerente.
         Gerente gerente = new Gerente();
-
+        // cadastrar novo funcionario
         gerente.cadastrarFuncionarios("Nelson", "Rua quinto dos inferno, 123", "45 98838-5213", "nelson@hotmail.com", "jovem aprendiz", 500);
 
-        System.out.println("Funcionarios cadastrados com sucesso!");       
-    }   
-}
+        System.out.println("Funcionarios cadastrados com sucesso!");
+    } // fim metodo main
+} // fim classe UsaGerente
