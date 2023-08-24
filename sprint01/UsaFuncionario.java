209b9154ba285402c3b4489013ff433d773b9aef
pedrosch <98292262+pedrobolfe@users.noreply.github.com>
2023-08-24 15:11:23 -0300

    Update UsaFuncionario.java

--- sprint01/UsaFuncionario.java
+++ sprint01/UsaFuncionario.java
@@ -2,28 +2,17 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class UsaFuncionario {
-
     public static void main(String[] args) {
-        Funcionario funcionario1 = new Funcionario("Peter Parker", "peterparker@gmail.com", "Queens, New York", "915633874", "Secretário",1200.65);
+        Funcionario funcionario1 = new Funcionario("Peter Parker", "peterparker@gmail.com", "Queens, New York", "915633874", "Secretário", 1200.65);
 
         ArrayList<String> dependentes_lista = new ArrayList<String>(Arrays.asList("dependente1", "dependente2", "dependente3"));
-        ArrayList<String> dadosPagemento_lista = new ArrayList<String>(Arrays.asList("true","pix"));
+        ArrayList<String> dadosPagemento_lista = new ArrayList<String>(Arrays.asList("true", "pix"));
 
-        funcionario1.cadastrarAssociado("bruce banner","banner@hotmail.com","avenida das pombas","69357841",dadosPagemento_lista,dependentes_lista);
+        funcionario1.cadastrarAssociado("bruce banner","banner@hotmail.com","avenida das pombas","69357841", dadosPagemento_lista, dependentes_lista);
 
-    
-        funcionario1.cadastrarAssociado("kurt kobain","banner@hotmail.com","avenida das pombas","69357841",dadosPagemento_lista,dependentes_lista);
+        funcionario1.cadastrarAssociado("kurt kobain","banner@hotmail.com","avenida das pombas","69357841", dadosPagemento_lista, dependentes_lista);
 
         String cadastrados = funcionario1.getAssociadosCadastrados();
-        System.out.println(cadastrados);
-
-            
-        }
-
-        
-        
-
-
-
+        System.out.println(cadastrados);        
     }
-    
+}
