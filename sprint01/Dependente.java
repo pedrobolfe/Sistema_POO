e7ca5afa632564a3c8f3292d01aef65364670c2e
Eliseu <124800294+EliseuDLS@users.noreply.github.com>
2023-08-29 15:36:05 -0300

    Update Dependente.java

--- sprint01/Dependente.java
+++ sprint01/Dependente.java
@@ -1,11 +1,20 @@
 import java.util.ArrayList;
+public class Dependente extends Pessoa{
 
-public class Dependente extends Associado{
+    public int idade;
 
-    public Dependente(String nome, String email, String endereco, String telefone, String dadosPagemento,
-        String dependentes, ArrayList<String> atividadesEsportivas) {
-        super(nome, email, endereco, telefone, dadosPagemento, dependentes, atividadesEsportivas);
-        //TODO Auto-generated constructor stub
+    public Dependente(String nome,  String email, String endereco, String telefone, int idade){
+        super(nome, email, telefone, endereco);
+        this.idade = idade;
     }
     
+    //getters
+    public int getIdade() {
+        return idade;
+    }
+   
+    //setters
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
 }
