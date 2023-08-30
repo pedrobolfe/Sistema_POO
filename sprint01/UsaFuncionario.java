5be501bdc2f0f3558b8876e53af7fb8f65f47584
pedrosch <98292262+pedrobolfe@users.noreply.github.com>
2023-08-30 17:31:25 -0300

    Update UsaFuncionario.java

--- sprint01/UsaFuncionario.java
+++ sprint01/UsaFuncionario.java
@@ -2,15 +2,15 @@ import java.util.ArrayList;
 
 public class UsaFuncionario {
     public static void main(String[] args) {
-        Funcionario funcionario1 = new Funcionario("Peter Parker", "peterparker@gmail.com", "Queens, New York", "915633874", "Secretário", 1200.65);
-        Funcionario funcionario2 = new Funcionario("Peter Parker", "peterparker@gmail.com", "Queens, New York", "915633874", "GERENTE", 1200.65);
+        Funcionario funcionario1 = new Funcionario("Gedervaldo Cunha Silveira", "gedervaldocunha45@gmail.com", "Rua Inacio, 791", "98856-7845", "Secretário", 1200.65);
+        Funcionario funcionario2 = new Funcionario("Adelinha de Carvalho Espernça", "linacarvalho7@gmail.com", "Rua Tupiaçi, 1246", "98889-7416", "GERENTE", 2648.49);
 
-        Dependente dependentes = new Dependente("Pedroo", "miotto", "rua", "666", 10);
-        Dependente dependentes2 = new Dependente("Pedroo", "miotto", "rua", "666",10);
+        Dependente dependentes = new Dependente("Pedro Miotto", "miottopedro12@gmail.com", "Rua Finaldo Cunha, 49", "98846-15748", 14);
+        Dependente dependentes2 = new Dependente("Clebeia Costa da Luz", "leialuz999@hotmail.com", "Rua Euseila, 798", "98846-1556", 9);
         ArrayList<Dependente> dependentesLista= new ArrayList<Dependente>();
         dependentesLista.add(dependentes);
         ArrayList<String> atividadesEsportivas= new ArrayList<String>();
-        atividadesEsportivas.add("hello there");
+        atividadesEsportivas.add("natação");
 
         funcionario1.cadastrarAssociado("a", "aaaaa", "aaa", "aaaaaaaaaa", "pix", dependentesLista, atividadesEsportivas);
         funcionario1.cadastrarAssociado("pedro", "aaaaa", "aaa", "aaaaaaaaaa", "pix", dependentesLista, atividadesEsportivas);
